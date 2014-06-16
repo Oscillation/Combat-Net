c2ece26130c6d951334f65518d0ec612197b0237
Mntndew <trickepiclyawesome@gmail.com>
2014-06-16 10:02:45 +0200

    Fixed particle memory leak.

--- Client/Client/ParticleEmitter.cpp
+++ Client/Client/ParticleEmitter.cpp
@@ -21,27 +21,28 @@ void ParticleEmitter::update(sf::Time & p_deltaTime){
 			m_vertexArray.append(m_particles[i].trail->getVertex());
 		}else
 		{
+			delete m_particles[i].trail;
 			m_particles.erase(m_particles.begin() + i);
 		}
 	}
 }
 
 void ParticleEmitter::Emit(const std::string & p_type, const sf::Vector2<float> & p_position, const sf::Vector2<float> & p_velocity){
-	Particle* particle = new Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, p_velocity);
-	m_particles.push_back(*particle);
+	Particle particle = Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, p_velocity);
+	m_particles.push_back(particle);
 }
 
 void ParticleEmitter::Emit(const std::string & p_type, const sf::Vector2<float> & p_position){
 	sf::Vector2<float> velocity = sf::Vector2<float>((((float)math::random(0, 100))/100)*(math::random(-1, 1, 0)), (((float)math::random(0, 100))/100)*(math::random(-1, 1, 0)));
-	Particle* particle = new Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, velocity);
-	m_particles.push_back(*particle);
+	Particle particle = Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, velocity);
+	m_particles.push_back(particle);
 }
 
 void ParticleEmitter::Emit(const std::string & p_type, const sf::Vector2<float> & p_position, const sf::Vector2<float> & p_velocity, const int & p_amount){
 	for (int i = 0; i < p_amount; i++)
 	{
-		Particle* particle = new Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, p_velocity);
-		m_particles.push_back(*particle);
+		Particle particle = Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, p_velocity);
+		m_particles.push_back(particle);
 	}
 }
 
@@ -49,8 +50,8 @@ void ParticleEmitter::Emit(const std::string & p_type, const sf::Vector2<float> 
 	for (int i = 0; i < p_amount; i++)
 	{
 		sf::Vector2<float> velocity = sf::Vector2<float>(((((float)math::random(1, 100))/100)*(math::random(-1, 1, 0))), ((((float)math::random(1, 100))/100)*(math::random(-1, 1, 0))));
-		Particle* particle = new Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, velocity);
-		m_particles.push_back(*particle);
+		Particle particle = Particle(p_type, ptr_particleLoader->getParticleType(p_type), p_position, velocity);
+		m_particles.push_back(particle);
 	}
 }
 
